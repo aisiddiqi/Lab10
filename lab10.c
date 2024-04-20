04ba3eda79d38305661b5b30e47becf94641a883
aisiddiqi <114627009+aisiddiqi@users.noreply.github.com>
2024-04-20 08:19:49 -0400

    Update lab10.c

--- lab10.c
+++ lab10.c
@@ -2,139 +2,132 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Trie node structure
-struct TrieNode
-{
-    struct TrieNode *children[26];
-    int isEndOfWord;
+// Trie structure
+struct Trie
+{	
+    struct Trie *children[26];
+    int flag;
 };
 
-// Function to create a new Trie node
-struct TrieNode *createTrieNode()
+// Inserts the word to the trie structure
+void insert(struct Trie *pTrie, char *word)
 {
-    struct TrieNode *newNode = (struct TrieNode *)malloc(sizeof(struct TrieNode));
-    if (newNode)
+    int strLen = strlen(word);
+    struct Trie *temp = pTrie;
+    for (int i = 0; i < strLen; i++)
     {
-        newNode->isEndOfWord = 0;
-        for (int i = 0; i < 26; i++)
+        if (temp->children[word[i] - 'a'] == NULL)
         {
-            newNode->children[i] = NULL;
+            temp->children[word[i] - 'a'] = malloc(sizeof(char) * 256);
+            temp->children[word[i] - 'a']->flag = 0;
+            for (int j = 0; j < 26; j++)
+            {
+                temp->children[word[i] - 'a']->children[j] = NULL;
+            }
         }
+        temp = temp->children[word[i] - 'a'];
     }
-    return newNode;
+    temp->flag += 1;
+    
 }
 
-// Inserts the word into the trie structure
-void insert(struct TrieNode *root, char *word)
+// computes the number of occurances of the word
+int numberOfOccurances(struct Trie *pTrie, char *word)
 {
-    struct TrieNode *crawler = root;
-    for (int i = 0; i < strlen(word) - 1; i++)
+    int strLen = strlen(word);
+    struct Trie *temp = pTrie;
+    for (int i = 0; i < strLen; i++)
     {
-        int index = word[i] - 'a';
-        if (crawler->children[index] == NULL)
-        {
-            crawler->children[index] = createTrieNode();
-        }
-        crawler = crawler->children[index];
-    }
-    crawler->isEndOfWord += 1;
-}
-
-// Helper function for counting occurrences
-int countOccurrences(struct TrieNode *root, char *word)
-{
-    struct TrieNode *crawler = root;
-    for (int i = 0; i < strlen(word); i++)
-    {
-        int index = word[i] - 'a';
-        if (!crawler->children[index])
+        temp = temp->children[word[i] - 'a'];
+        if (temp == NULL)
         {
             return 0;
         }
-        crawler = crawler->children[index];
     }
-    return (crawler != NULL && crawler->isEndOfWord);
+    return temp->flag;
 }
 
-// Computes the number of occurrences of the word
-int numberOfOccurrences(struct TrieNode *root, char *word)
+// deallocate the trie structure
+struct Trie *deallocateTrie(struct Trie *pTrie)
 {
-    return countOccurrences(root, word);
-}
-
-// Deallocates the trie structure
-void deallocateTrie(struct TrieNode *root)
-{
-    if (root == NULL)
+    if (pTrie == NULL)
     {
-        return;
+        return NULL;
     }
+
     for (int i = 0; i < 26; i++)
     {
-        if (root->children[i] != NULL)
-        {
-            deallocateTrie(root->children[i]);
-        }
+        deallocateTrie(pTrie->children[i]);
     }
-    free(root);
+
+    free(pTrie);
+    return NULL;
 }
 
 // Initializes a trie structure
-struct TrieNode *createTrie()
+struct Trie *createTrie()
 {
-    return createTrieNode();
+    struct Trie *root = malloc(sizeof(struct Trie));
+    root->flag = 0;
+    for (int i = 0; i < 26; i++)
+    {
+        root->children[i] = NULL;
+    }
+    return root;
 }
 
-// Reads the dictionary file and stores the words
+// this function will return number of words in the dictionary,
+// and read all the words in the dictionary to the structure words
 int readDictionary(char *filename, char **pInWords)
 {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL)
+    FILE *inFile = fopen(filename, "r");
+    if (inFile == NULL)
     {
-        printf("Error opening file!\n");
-        return 0;
+        printf("File Not Found Error, Exiting Program...");
+        exit(1);
+    }
+    int size;
+    fscanf(inFile, "%d ", &size);
+    char buffer[256];
+
+
+    for (int i = 0; i < size; i++) 
+    {
+        pInWords[i] = (char *)malloc(256 * sizeof(char));
     }
 
-    int numWords = 0;
-    char word[100]; // Assuming max word length is 99 characters
-    while (fscanf(file, "%s", word) != EOF)
+    for (int i = 0; i < size; i++)
     {
-        pInWords[numWords] = strdup(word);
-        numWords++;
+        fscanf(inFile, "%s ", buffer);
+        strcpy((pInWords[i]), buffer);
     }
-    fclose(file);
-    return numWords;
+    return size;
 }
 
 int main(void)
 {
-    char *inWords[256];
-
-    // Read the number of words in the dictionary
-    int numWords = readDictionary("C:\\Users\\aisid\\Downloads\\Lab10\\dictionary.txt", inWords);
-    for (int i = 0; i < numWords; ++i)
-    {
-        printf("%s\n", inWords[i]);
-    }
-
-    // Create and populate the Trie
-    struct TrieNode *pTrie = createTrie();
-    for (int i = 0; i < numWords; i++)
-    {
-        insert(pTrie, inWords[i]);
-    }
-
-    // Test with some sample words
-    char *pWords[] = {"notaword", "ucf", "no", "note", "corg"};
-    for (int i = 0; i < 5; i++)
-    {
-        printf("\t%s : %d\n", pWords[i], numberOfOccurrences(pTrie, pWords[i]));
-    }
-
-    // Deallocate the Trie
-    deallocateTrie(pTrie);
-    if (pTrie != NULL)
-        printf("There is an error in this program\n");
-
-    return 0;
+	char *inWords[256];
+	
+	//read the number of the words in the dictionary
+	int numWords = readDictionary("C:\\Users\\aisid\\Downloads\\Lab10\\dictionary.txt", inWords);
+	for (int i=0;i<numWords;++i)
+	{
+		printf("%s\n",inWords[i]);
+	}
+	
+	struct Trie *pTrie = createTrie();
+	for (int i=0;i<numWords;i++)
+	{
+		insert(pTrie, inWords[i]);
+	}
+	// parse lineby line, and insert each word to the trie data structure
+	char *pWords[] = {"notaword", "ucf", "no", "note", "corg"};
+	for (int i=0;i<5;i++)
+	{
+		printf("\t%s : %d\n", pWords[i], numberOfOccurances(pTrie, pWords[i]));
+	}
+	pTrie = deallocateTrie(pTrie);
+	if (pTrie != NULL)
+		printf("There is an error in this program\n");
+	return 0;
 }
